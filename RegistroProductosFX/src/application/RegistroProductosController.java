6b41565eb3e8ae2cf342492e53d87bc47f6d1529
Erick Vladimir Reyes Marín <erick.marin.v@gmail.com>
2015-06-23 23:29:35 -0600

    Cambios varios

--- RegistroProductosFX/src/application/RegistroProductosController.java
+++ RegistroProductosFX/src/application/RegistroProductosController.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -8,8 +9,25 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import modelo.Producto;
 
 public class RegistroProductosController implements Initializable{
+	@FXML private TextField txtCodigoProducto; 
+	@FXML private TextField txtCodigoBarras; 
+	@FXML private TextField txtNombreProducto;
+	@FXML private TextArea txtDescripcion;
+	@FXML private TextField txtPrecioCompra; 
+	@FXML private TextField txtPrecioVenta; 
+	@FXML private TextField txtExistencia;
+	@FXML private DatePicker dpFechaVencimiento;
+	@FXML private RadioButton rbtLempiras;
+	@FXML private RadioButton rbtDolares;
+	@FXML private TextArea txtResultado;
+	
 	@FXML private ComboBox<String> cboMarca;
 	@FXML private ComboBox<String> cboLote;
 	@FXML private ComboBox<String> cboCategoria;
@@ -22,7 +40,7 @@ public class RegistroProductosController implements Initializable{
 	private ObservableList<String> listaUnidadesMedidas;
 	private ObservableList<String> listaProveedores;
 	
-	
+	private ObservableList<Producto> informacion;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -31,6 +49,7 @@ public class RegistroProductosController implements Initializable{
 		listaCategorias = FXCollections.observableArrayList();
 		listaUnidadesMedidas = FXCollections.observableArrayList();
 		listaProveedores = FXCollections.observableArrayList();
+		informacion = FXCollections.observableArrayList();
 		
 		llenarListas();
 		cboMarca.setItems(listaMarcas);
@@ -38,6 +57,8 @@ public class RegistroProductosController implements Initializable{
 		cboCategoria.setItems(listaCategorias);
 		cboUnidadMedida.setItems(listaUnidadesMedidas);
 		cboProveedor.setItems(listaProveedores);
+
+		
 	}
 	
 	public void llenarListas(){
@@ -60,6 +81,39 @@ public class RegistroProductosController implements Initializable{
 		listaProveedores.add("Proveedor 1");
 		listaProveedores.add("Proveedor 2");
 		listaProveedores.add("Proveedor 3");
+	}
+	
+	@FXML
+	public void agregarRegistro(){
+		/*Alert cuadroDialogo = new Alert(AlertType.INFORMATION);
+		cuadroDialogo.setTitle("");
+		cuadroDialogo.setContent("");
+		cuadroDialogo.showAndWait();*/
+		informacion.add(
+				new Producto(
+					Integer.valueOf(txtCodigoProducto.getText()),
+					txtCodigoBarras.getText(),
+					txtNombreProducto.getText(),
+					txtDescripcion.getText(),
+					Double.valueOf(txtPrecioCompra.getText()),
+					Double.valueOf(txtPrecioVenta.getText()),
+					cboMarca.getSelectionModel().getSelectedItem(),
+					cboLote.getSelectionModel().getSelectedItem(),
+					cboCategoria.getSelectionModel().getSelectedItem(),
+					cboUnidadMedida.getSelectionModel().getSelectedItem(),
+					cboProveedor.getSelectionModel().getSelectedItem(),
+					Float.valueOf(txtExistencia.getText()),
+					new Date(dpFechaVencimiento.getValue().toEpochDay()),
+					rbtLempiras.isSelected()?rbtLempiras.getText():rbtDolares.getText()
+				)
+		);
+		actualizarResultado();
+	}
+	
+	public void actualizarResultado(){
+		txtResultado.setText(null);
+		for(int i=0; i<informacion.size(); i++)
+			txtResultado.appendText(informacion.get(i).toString());
 	}
 	
 }
